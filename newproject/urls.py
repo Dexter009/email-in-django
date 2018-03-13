a8ba2737c20bcbec46732d31340032b8f85dbdfe
Dhiraj Gajurel <dhirazgazurel@gmail.com>
2018-03-13 21:35:59 +0545

    hello

--- newproject/urls.py
+++ newproject/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import myapp
 from myapp import views
 
 urlpatterns = [
@@ -9,5 +10,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'home/',views.index,name='index'),
+    url(r'home/',myapp.views.index,name='index'),
 ]
