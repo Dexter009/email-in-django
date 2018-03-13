fb597f01bee898e48bc6728c02a1e7f1d665b93f
Dhiraj Gajurel <dhirazgazurel@gmail.com>
2018-03-13 20:46:36 +0545

    gain some changes

--- newproject/settings.py
+++ newproject/settings.py
@@ -104,6 +104,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
