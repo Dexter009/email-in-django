5715f475ea93400a9ca3308a09867b6cd0381a49
Dhiraj Gajurel <dhirazgazurel@gmail.com>
2018-03-13 21:40:46 +0545

    i guess this is final

--- newproject/urls.py
+++ newproject/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-import myapp
-from myapp import views
+import myapp.views
+
+admin.autodiscover()
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'newproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'home/',myapp.views.index,name='index'),
+    url(r'^admin/',admin.site.urls),
+    url(r'^$', myapp.views.index,name='index'),
 ]
