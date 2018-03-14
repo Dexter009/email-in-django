e79003d8dd01d7ae0e0b9ba024878740453e5d1a
Dhiraj Gajurel <dhirazgazurel@gmail.com>
2018-03-14 19:07:47 +0545

    heirroo

--- newproject/settings.py
+++ newproject/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
+        
 
     }
 }
