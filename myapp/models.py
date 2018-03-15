5228437a931ea8d0e68654a09e79c3f748fca362
Dhiraj Gajurel <dhirazgazurel@gmail.com>
2018-03-15 20:37:21 +0545

    Heroku deploy ready1

--- myapp/models.py
+++ myapp/models.py
@@ -5,4 +5,4 @@ class Question(models.Model):
     name=models.CharField(max_length=255)
     age=models.CharField(max_length=10)
     height=models.CharField(max_length=15)
-    image = models.FileField()
+    image = models.FileField(blank=True,null=True)
--- newproject/settings.py
+++ newproject/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'myapp/static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 
