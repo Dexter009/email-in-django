11ccf6aef14b803d0e228c58d8665032b08edf4a
Dhiraj Gajurel <dhirazgazurel@gmail.com>
2018-03-15 21:52:53 +0545

    Heroku deploy ready6

--- newproject/settings.py
+++ newproject/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = (
     'myapp',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -53,7 +53,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-)
+]
 
 ROOT_URLCONF = 'newproject.urls'
 
